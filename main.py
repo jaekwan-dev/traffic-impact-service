8f291b9a37988d90d3dfb2185f66983cc29d3674
jaekwan-dev <jaekwan.yoodev@gmail.com>
2025-06-14 17:41:10 +0900

    add db connection

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from sqlalchemy import create_engine, text
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
 
+DATABASE_URL = (
+    f"postgresql://{os.getenv('DB_USER')}:"
+    f"{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:"
+    f"{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+)
+
+engine = create_engine(DATABASE_URL)
+
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    with engine.connect() as connection:
+        result = connection.execute(text("SELECT 1;"))
+        db_status = result.scalar()
+    return {"Hello": "World", "db_status": db_status}
